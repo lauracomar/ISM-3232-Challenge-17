b421229d21a9436ec50dd58f0d937d042b37ec93
Laura Comar <lautmcomar@gmail.com>
2024-11-24 12:19:53 -0500

    Style components for better user experience(fixed api error)

--- tour-comparison-app/src/components/Gallery.jsx
+++ tour-comparison-app/src/components/Gallery.jsx
@@ -9,7 +9,7 @@ const Gallery = () => { // display data
     useEffect(() => {
         const fetchTours = async () => { //function to fetch data
             try {
-                const response = await fetch("https://course-api.com/react-tours-project"); // call API 
+                const response = await fetch("https://www.course-api.com/react-tours-project"); // call API 
                 if (!response.ok) {
                     throw new Error('Error: fetch failed'); // Handle and display an error message if the fetch fails
                 }
@@ -17,7 +17,6 @@ const Gallery = () => { // display data
                 settours(data); // data fetched into state
                 setLoading(false); //loading to false after fetching data
             } catch (error) {
-                console.error("Error", error); //log full error
                 setError(error.message); //error message if fetch fails
                 setLoading(false); //loading to false to stop
             }
@@ -28,7 +27,7 @@ const Gallery = () => { // display data
         settours(tours.filter((tour) => tour.id !== id)); //remove tour (using filter = kind of filtering out the tour)
     };
     if (loading) return <p>Loading...</p> // render loading
-    if (error) return <p>Error</p> // render error
+    if (error) return <p>Error: {error}</p> // render error
     return (
         <div className="gallery-container">
             {tours.map((tour) => (
