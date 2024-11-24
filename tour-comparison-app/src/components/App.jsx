5beaf22ae31a1be6e1a53f6a9150280b974213be
Laura Comar <lautmcomar@gmail.com>
2024-11-24 10:53:25 -0500

    "Setup root component with global state and routing."

---
+++ tour-comparison-app/src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from "react"; //import react
+import Gallery from "./Gallery"; //import gallery - show tours 
+
+const App = () => {
+  return (
+    <div>
+      <h2>Tour Comparison App</h2>
+      <Gallery />
+    </div>
+  );
+};
+export default App; //export app to render in main
